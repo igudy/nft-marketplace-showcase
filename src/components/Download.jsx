eb2584d8918566d577432198a1caa7404411feab
igudy <goodnessigunma1@gmail.com>
2024-08-14 22:22:51 +0100

    feat: source code for github added

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -1,19 +1,27 @@
-import React from 'react';
+import React from "react";
 
-import styles from '../styles/Global';
-import assets from '../assets';
+import styles from "../styles/Global";
+import assets from "../assets";
 
 const Download = () => {
   return (
     <div className={`${styles.section} ${styles.bgWhite}`}>
       <div className={`${styles.subSection} flex-col text-center`}>
         <div>
-          <h1 className={`${styles.h1Text} ${styles.blackText}`}>Download the Source Code</h1>
-          <p className={`${styles.pText} ${styles.blackText}`}>Get the full source code on GitHub</p>
+          <h1 className={`${styles.h1Text} ${styles.blackText}`}>
+            Download the Source Code
+          </h1>
+          <p className={`${styles.pText} ${styles.blackText}`}>
+            Get the full source code on GitHub
+          </p>
         </div>
-        <button className={styles.btnPrimary}>Source Code</button>
+        <button className={styles.btnPrimary}>
+          <a href="https://github.com/igudy/nft-marketplace-showcase.git">
+            Source Code
+          </a>
+        </button>
         <div className={styles.flexCenter}>
-          <img 
+          <img
             src={assets.scene}
             alt="download_png"
             className={styles.fullImg}
@@ -21,7 +29,7 @@ const Download = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Download
+export default Download;
